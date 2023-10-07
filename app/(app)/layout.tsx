78c7648479a234440ee185e5c3b0374b3d28481b
carlo moleka <me@carlomoleka.com>
2023-10-07 16:12:10 -0400

    style(component): add border to site-footer and change border color to dashboard layout

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -20,7 +20,7 @@ export default async function AppLayout({ children }: LayoutProps) {
   return (
     <div className="flex overflow-hidden">
       <aside className="hidden lg:block">
-        <div className="relative hidden h-screen w-2/12 max-w-xs flex-row justify-center border-r border-slate-100 py-4 dark:border-slate-700 sm:min-w-[12rem] lg:flex lg:min-w-[17rem]">
+        <div className="relative hidden h-screen w-2/12 max-w-xs flex-row justify-center border-r py-4  sm:min-w-[12rem] lg:flex lg:min-w-[17rem]">
           <div className="flex w-full flex-col space-y-7 px-4">
             <NavBar userId={user.id} />
           </div>
--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@/components/ui/typography"
 
 export function SiteFooter() {
   return (
-    <footer className="py-6 md:px-8 md:py-0">
+    <footer className="py-6 md:px-8 md:py-0 border-t">
       <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
         <Typography
           variant="sm"
